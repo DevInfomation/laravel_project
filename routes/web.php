d08858a19abf8598136d86c467039d993edcd9a5
DevInfo <stefanmilkovski99@gmail.com>
2023-11-05 22:33:14 +0100

    Creating a database and using migrations table

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,17 @@ use App\Models\Listing;
 |
 */
 
+// All Listing
 Route::get('/', function () {
     return view('listings', [
         'heading' => 'Latest Listings',
         'listings' => Listing::all() 
+    ]);
+});
+
+// Single Listing
+Route::get('/listings/{id}', function($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
     ]);
 });
